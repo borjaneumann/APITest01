499fc5af4348b7da495420e77e63298acbbe26a1
Borja Neumann <borjaneumann@hotmail.com>
2021-09-22 10:09:23 +0200

    Preloading the data base

---
+++ src/main/java/restAPI/REST/API/test1/payroll/LoadDatabase.java
@@ -0,0 +1,22 @@
+package restAPI.REST.API.test1.payroll;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+class LoadDatabase {
+
+    private static final Logger log = LoggerFactory.getLogger(LoadDatabase.class);
+
+    @Bean
+    CommandLineRunner initDatabase(EmployeeRepository repository) {
+
+        return args -> {
+            log.info("Preloading " + repository.save(new Employee("John Smith", "Cheerleader")));
+            log.info("Preloading " + repository.save(new Employee("Jan Smit", "mid-fielder")));
+        };
+    }
+}
